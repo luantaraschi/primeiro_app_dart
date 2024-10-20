af4ad9f1c2f5403a08660efe848e53b44416979e
luantaraschi <luantaraschi@gmail.com>
2024-10-20 10:21:04 -0300

    print adicionado

--- bin/primeiro_app.dart
+++ bin/primeiro_app.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
 void main() {
-  print("Hello World!");
-}
+  print("Ola me chamo Dart. Qual seu nome?");
+  var entrada = stdin.readLineSync();
+  print("Prazer em te conhecer $entrada, vamos fazer varios programas juntos!");
+  }
